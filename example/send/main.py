eaf97052259a034ba9943a3cd23c1afe2e937729
Peikai Xu <xupeikai0132@sjtu.edu.cn>
2025-08-31 14:49:28 +0000

    feat: send 10 times

--- example/send/main.py
+++ example/send/main.py
@@ -4,11 +4,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 from my_meshtastic.loader import load_config
 from my_meshtastic.message.send import send_message
+import time
 
 def main():
     devinfo=load_config("config/device.yaml")
     print(devinfo)
-    send_message(devinfo['dev1'], "Hello", devinfo['dev2_id'])
+    for i in range(10):
+        send_message(devinfo['dev1'], "Hello", devinfo['dev2_id'])
+        time.sleep(1)
     
 
 
